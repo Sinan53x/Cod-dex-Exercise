f530338c3c63a95a5e8b3494a73979604c9d9dbb
Sinan53x <sinan.turna@yahoo.com>
2025-07-30 10:31:59 +0200

    write liked songs into txt file

---
+++ Intermediate Python 07. The File Realm/likedsongs.py
@@ -0,0 +1,13 @@
+liked_songs = {
+  'Bad Habits': 'Ed Sheeran',
+  "I'm Just Ken": 'Ryan Gosling',
+  'Mastermind': 'Taylor Swift',
+  'Uptown Funk': 'Mark Ronson ft. Bruno Mars',
+  'Ghost': 'Justin Bieber'
+}
+
+def write_liked_songs_to_file(songs, file_name):
+    with open(file_name, 'w') as file:
+        file.write('Liked Songs:\n')
+        for song, artist in songs.items():
+            file.write(f' {song} by {artist}\n')
