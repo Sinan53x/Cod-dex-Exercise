8e56599dda4730fcd039c68424dabee4ba5663b8
Sinan53x <sinan.turna@yahoo.com>
2025-07-31 12:50:02 +0200

    .

--- Intermediate Python  08. Uncharted Files/sent_message.py
+++ Intermediate Python  08. Uncharted Files/sent_message.py
@@ -1,4 +1,8 @@
 sent_message = 'Hey there! This is a secret message.'
 
-with open('sent_message.txt', 'w') as file:
-  file.write(sent_message)
+with open('sent_message.txt', 'r+') as file:
+  # Read the sent message from the file
+  original_message = file.read()
+      
+  # Move the cursor to the beginning of the file
+  file.seek(0)
